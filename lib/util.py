ce5bd7920982350bf4fc90329a09fcde9b132a96
ogahector <hector.oga22@imperial.ac.uk>
2024-06-23 18:27:17 +0100

    beginning util

--- lib/util.py
+++ lib/util.py
@@ -1,1 +1,19 @@
+import math as m
+import os
+import numpy as np
+import matplotlib as plot
 
+# CLC 
+def clc():
+    os.system("cls" if os.name=='nt' else 'clc failed')
+
+# EYE
+def eye(n):
+    return [ [(1. if y == x else 0.) for y in range(n)] for x in range(n)]
+
+# LINSPACE
+def linspace(a: float, b: float, n: int):
+    return [ (a + i*(b-a)/n) for i in range(0, n+1)]
+
+print(linspace(1, 10, 10))
+print(np.linspace(1, 10, 10))
