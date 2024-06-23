b72144fac327e8ce448375cd8e798382f102aec5
ogahector <hector.oga22@imperial.ac.uk>
2024-06-23 21:52:59 +0100

    stuffs

--- lib/util.py
+++ lib/util.py
@@ -1,7 +1,7 @@
 import math as m
 import os
 import numpy as np
-import matplotlib as plot
+import matplotlib.pyplot as plt
 
 # CLC 
 def clc():
@@ -9,11 +9,37 @@ def clc():
 
 # EYE
 def eye(n):
-    return [ [(1. if y == x else 0.) for y in range(n)] for x in range(n)]
+    return np.array([ [(1. if y == x else 0.) for y in range(n)] for x in range(n)])
 
 # LINSPACE
 def linspace(a: float, b: float, n: int):
-    return [ (a + i*(b-a)/n) for i in range(0, n+1)]
+    return np.array([ (a + i*(b-a)/(n-1)) for i in range(0, n)])
 
-print(linspace(1, 10, 10))
-print(np.linspace(1, 10, 10))
+# LOGSPACE
+def logspace(a: float, b: float, n: int):
+    # return [ a*(i*(b-a)/(n-1)) for i in range(0, n)]
+    return np.logspace(a,b,n)
+
+# ZEROS
+def zeros(m, n):
+    return np.array([[0. for columns in range(n)] for rows in range(m)])
+
+# ONES
+def ones(m, n):
+    # return np.array([[1 for columns in range(n)] for rows in range(m)])
+    return zeros(m, n) + 1.
+
+# DIAG
+def diag(x):
+    if(x.ndim == 1): return np.array([ [x[i] if i == j else 0. for i in range(x.shape[0])] for j in range(x.shape[0])])
+    else: return np.array([x[i][i] for i in range(x.shape[1])])
+
+# RAND
+def rand(m, n):
+    return np.array([[np.random.randint(1, 100) for y in range(n)] for x in range(m)])
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
