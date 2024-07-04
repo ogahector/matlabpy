0527e45306eecbf50c8a162eb38c7056cc385894
ogahector <hector.oga22@imperial.ac.uk>
2024-07-05 00:03:07 +0100

    started external variable storage

--- lib/util.py
+++ lib/util.py
@@ -2,7 +2,45 @@ import math as m
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle as p
 
+### CLASSES:
+class MATStruct:
+    def __init__(self, d: dict) -> None:
+        for entry in d.keys():
+            setattr(self, entry, d[entry])
+
+    def __add__(self, other):
+        for other_names in vars(other).keys():
+            setattr(self, other_names, vars(other)[other_names])
+        return self
+
+    def delete(self, name: str) -> None:
+        if name in dir(self): del vars(self)[name]
+        else: print("No variable with the name " + name + " was found!")
+
+    def add(self, name: str, obj) -> None:
+        setattr(self, name, obj)
+
+
+colors1 = {
+    "blue" : 5,
+    "red" : 6,
+    "yellow" : 7,
+    }
+
+colors2 = {
+    "green" : 8,
+    "purple" : 9,
+    "black" : 10,
+    }
+
+a = MATStruct(colors1)
+b = MATStruct(colors2)
+# print(vars(a+b))
+# print(vars(b))
+
+### FUNCTIONS:
 # CLC 
 def clc():
     os.system("cls" if os.name=='nt' else 'clc failed')
@@ -38,8 +76,80 @@ def diag(x):
 def rand(m, n):
     return np.array([[np.random.randint(1, 100) for y in range(n)] for x in range(m)])
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+# MAKE FILE TO STORE VARIABLES
+def create_file(filename: str):
+    open(filename, 'x') # only creates the file
+    print("File Created Successfully!")
 
-plt.plot(xpoints, ypoints)
-plt.show()
+# WRITE GLOBALS TO FILE
+def write(filename: str, vars=[], overwrite=False) -> None:
+    with open(filename, 'ab' if not overwrite else 'wb') as file:
+        d = {}
+        if vars == []:
+            for name, val in globals().items(): d.update({name: val})
+        else:
+            for name, val in vars: d.update({name: val})
+        p.dump(d, file)
+
+
+# READ FROM FILE
+def load(filename: str):
+    with open(filename, 'rb') as file:
+        d = load(file)
+        return MATStruct(d)
+
+print(globals())
+
+# a = 1
+# b = 1.0
+# c = "1"
+# d = '1'
+# e = [1, 2, 3]
+# f = (1, 2, 3)
+# g = {'a': 1, 'b': 2, 'c': 3}
+
+# # create_file("test.txt")
+# write("test.txt")
+# S = load("test.txt")
+# print(vars(S))
+
+
+
+### ----------------------------------------------------------------------------------###
+# 
+def pfd(n: int) -> list[int]:
+    ret = []
+    i = n
+    while i % 2 == 0:
+        i /= 2
+        ret.append(2)
+
+    return ret + [i for i in range(2, int(m.sqrt(n) + 1), 2) if n % i == 0]
+
+    for i in range(3, int(m.sqrt(n)), 2):
+        while n % i == 0:
+            n /= i
+            ret.append(i)
+                 
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        maxs = []
+        for i in range(len(nums)):
+            temp = []
+            for j in range(i, len(nums)):
+                temp.append(nums[j])
+                maxs.append(sum(temp))
+        return max(maxs)
+
+    def findMaximumXOR(self, nums: list[int]) -> int:
+        return max([nums[j] ^ nums[i] for i in range(len(nums)) for j in range(i, len(nums))])
+    
+    
